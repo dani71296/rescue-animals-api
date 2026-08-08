8982135a5aa2c8fa3c12026f61eee2466b969ffe
Guille-coder <guillermoreyen.16@gmail.com>
2026-08-08 13:57:26 -0500

    validacion

--- src/routes/adoptionRoutes.js
+++ src/routes/adoptionRoutes.js
@@ -2,13 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 const adoptionController = require('../controllers/adoptionController');
-const isAuthenticated = require('../middleware/authenticate.js');
-
+const adoptionValidation = require('../middleware/adoptionValidation');
+const validateRequest = require('../middleware/validateRequest');
 
 router.get('/', adoptionController.getAllAdoptions);
+
 router.get('/:id', adoptionController.getAdoptionById);
-router.post('/',isAuthenticated, adoptionController.createAdoption);
-router.put('/:id',isAuthenticated, adoptionController.updateAdoption);
-router.delete('/:id',isAuthenticated, adoptionController.deleteAdoption);
+
+router.post('/',adoptionValidation,validateRequest,adoptionController.createAdoption);
+
+router.put('/:id',adoptionValidation,validateRequest,adoptionController.updateAdoption);
+
+router.delete('/:id', adoptionController.deleteAdoption);
 
 module.exports = router;
