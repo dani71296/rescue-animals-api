55760ff7339aadd09433930a85d6b723bbdeae2d
SAAC7 <saulabraham881@gmail.com>
2026-08-08 23:14:23 -0600

    FIX: I added them back to the authentication routes.

--- src/routes/adoptionRoutes.js
+++ src/routes/adoptionRoutes.js
@@ -4,15 +4,17 @@ const router = express.Router();
 const adoptionController = require('../controllers/adoptionController');
 const adoptionValidation = require('../middleware/adoptionValidation');
 const validateRequest = require('../middleware/validateRequest');
+const isAuthenticated = require('../middleware/authenticate.js');
+
 
 router.get('/', adoptionController.getAllAdoptions);
 
 router.get('/:id', adoptionController.getAdoptionById);
 
-router.post('/',adoptionValidation,validateRequest,adoptionController.createAdoption);
+router.post('/',isAuthenticated,adoptionValidation,validateRequest,adoptionController.createAdoption);
 
-router.put('/:id',adoptionValidation,validateRequest,adoptionController.updateAdoption);
+router.put('/:id',isAuthenticated,adoptionValidation,validateRequest,adoptionController.updateAdoption);
 
-router.delete('/:id', adoptionController.deleteAdoption);
+router.delete('/:id',isAuthenticated,adoptionController.deleteAdoption);
 
 module.exports = router;
